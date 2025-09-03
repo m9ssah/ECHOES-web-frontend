b1cb455883b3017cb434a46efcdc1734cd21fbbf
Meemom <jemima.silaen@gmail.com>
2025-09-02 23:56:06 -0400

    added profile page

---
+++ ECHOES-WEB/app/(tabs)/ProfilePage.tsx
@@ -0,0 +1,20 @@
+import React, { useState, useRef } from 'react';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Dimensions,
+  TouchableOpacity,
+  Image,
+  StatusBar,
+  NativeSyntheticEvent,
+  NativeScrollEvent,
+  FlatList,
+} from 'react-native';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { useThemeColor } from '@/hooks/useThemeColor';
+import { Ionicons } from '@expo/vector-icons';
+import Svg, { Defs, RadialGradient, Stop, Circle } from 'react-native-svg';
+import { useRouter } from 'expo-router';
